e6d340a31e374add73957f42beef5d01aa1a4dd0
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-01-29 07:43:27 +0100

    Comit door interaction

---
+++ Assets/Door Interaction.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DoorInteraction : MonoBehaviour
+{
+    private bool enter = false;
+    public GameObject door;
+    private Vector2 teleportPosition = Vector2.zero;
+    private void Update()
+    {
+        if (enter && Input.GetKeyDown(KeyCode.E))
+        {
+            Teleport();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Door"))
+        {
+            door = collision.gameObject;
+            Enlace link = collision.gameObject.GetComponent<Enlace>();
+            if (link != null)
+            {
+                teleportPosition = link.GetTeleportPosition();
+                enter = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Door"))
+        {
+            door = null;
+            enter = false;
+        }
+    }
+
+    private void Teleport()
+    {
+        if (door != null) 
+        {
+            transform.position = teleportPosition;
+            door = null;
+            enter = false;
+        }
+    }
+}
