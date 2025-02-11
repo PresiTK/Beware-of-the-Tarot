a1212b1ea9b893d53f4141051946542ecf525d93
Yuhao David Xia <yuhao.xia@enti.cat>
2025-02-11 09:40:29 +0100

    Sprinting

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public enum Direction { NONE, UP, DOWN, LEFT, RIGHT };
 public class CharacterMovement : MonoBehaviour
 {
-    public float speedX = 1;
-    public float speedY = 1;
+    public float currentSpeedX;
+    public float currentSpeedY;
+    public float speedX = 1f;
+    public float speedY = 1f;
+    public float runningSpeedX = 1.5f;
+    public float runningSpeedY = 1.5f;
 
     Rigidbody2D rb2d;
 
     public Direction direction = Direction.NONE;
-
-    public float currShootTime = 0;
-    public float shootCadenceTime = 2.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -25,7 +26,6 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         UpdateDirection();
-        Shoot();
     }
 
     private void FixedUpdate()
@@ -55,7 +55,7 @@ public class CharacterMovement : MonoBehaviour
         }
 
         //Recordad multiplicar por deltaTime para que sea frame-dependent
-        rb2d.velocity = new Vector2(hInput * speedX * Time.fixedDeltaTime, vInput * speedY * Time.fixedDeltaTime);
+        rb2d.velocity = new Vector2(hInput * currentSpeedX * Time.fixedDeltaTime, vInput * currentSpeedY * Time.fixedDeltaTime);
     }
 
     private void UpdateDirection()
@@ -70,7 +70,6 @@ public class CharacterMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)){   horizontal -= 1;}
         if (Input.GetKey(KeyCode.RightArrow)){  horizontal += 1;}
 
-        
         if (vertical > 0){
             direction = Direction.UP;
         }else if (vertical < 0){
@@ -82,21 +81,16 @@ public class CharacterMovement : MonoBehaviour
         }else if (horizontal > 0){
             direction = Direction.RIGHT;
         }
-    }
 
-    private void Shoot(){   
-        if (Input.GetKeyDown(KeyCode.Space)){//Haced esto en lugar de una corrutina pls :(
-            currShootTime = 0;
-            Debug.Log("START SHOOTING");
-        }else if (Input.GetKey(KeyCode.Space)){
-            currShootTime += Time.deltaTime;
-            if (currShootTime > shootCadenceTime)
-            {
-                currShootTime = 0;
-                Debug.Log("CONTINUOUS SHOOTING");
-            }
-        }else{
-            currShootTime = 0;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeedX = speedX * runningSpeedX;
+            currentSpeedY = speedY * runningSpeedY;
+        }
+        else
+        {
+            currentSpeedX = speedX;
+            currentSpeedY = speedY;
         }
     }
 }
