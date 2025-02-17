d5e28fdad02ecc15ebad861b08d4cce03ef0900c
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-02-17 13:41:41 +0100

    ok

---
+++ Assets/Scripts/patrullar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrullar : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float distanciMinima;
+    private int numeroAleatorio;
+
+    private void Start()
+    {
+        numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[numeroAleatorio].position, velocidadMovimiento * Time.deltaTime);
+        if (Vector2.Distance(transform.position, puntosMovimiento[numeroAleatorio].position) < distanciMinima)
+        {
+            numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+        }
+    }
+}
