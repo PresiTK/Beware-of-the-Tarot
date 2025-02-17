975efaa1b03f58fbe824b12786c06d2cbf93a908
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2025-02-17 09:30:27 +0100

    juntnado

---
+++ Assets/Scripts added/GameOverScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject GameOver;
+
+    private void Start()
+    {
+        GameOver.SetActive(false);
+    }
+    public void GameOverMenu()
+    {
+        GameOver.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Yuhao");
+    }
+}
